af55939bef95b3575aef71f72e3682aefa277c07
mistermert504 <58525648+mistermert504@users.noreply.github.com>
2020-12-04 12:36:00 +0100

    Add files via upload

---
+++ CleanWindows.ps1
@@ -0,0 +1,108 @@
+﻿Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*3DBuilder*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*AppConnector*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingFinance*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingFoodAndDrink*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingHealthAndFitness*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingMaps*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingNews*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingSports*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingTranslator*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingTravel*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*BingWeather*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*FreshPaint*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*GetHelp*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Getstarted*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*HelpAndTips*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Messaging*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Microsoft3DViewer*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MicrosoftOfficeHub*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MicrosoftPowerBIForWindows*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MicrosoftSolitaireCollection*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MinecraftUWP*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MixedReality.Portal*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MoCamera*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*MSPaint*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*NetworkSpeedTest*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*OfficeLens*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Office.OneNote*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Office.Sway*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*OneConnect*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Print3D*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Reader*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*RemoteDesktop*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*SkypeApp*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Wallet*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Whiteboard*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsAlarms*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsCamera*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*windowscommunicationsapps*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsFeedbackHub*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsMaps*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsPhone*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Windows.Photos*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsReadingList*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsScan*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WindowsSoundRecorder*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WinJS.1.0*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*WinJS.2.0*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*YourPhone*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*ZuneMusic*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*ZuneVideo*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Viber*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*ACGMediaPlayer*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*EclipseManager*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*4DF9E0F8.Netflix*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*64885BlueEdge.OneCalendar*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*7EE7776C.LinkedInforWindows*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*828B5831.HiddenCityMysteryofShadows*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*89006A2E.AutodeskSketchBook*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*9E2F88E3.Twitter*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*A278AB0D.DisneyMagicKingdoms*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*A278AB0D.DragonManiaLegends*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*A278AB0D.MarchofEmpires*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*ActiproSoftwareLLC.562882FEEB491*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*AD2F1837.GettingStartedwithWindows8*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*AD2F1837.HPJumpStart*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*AD2F1837.HPRegistration*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*AdobeSystemsIncorporated.AdobePhotoshopExpress*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Amazon.com.Amazon*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*DropboxOEM*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Plex*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*CyberLinkCorp.hs.PowerMediaPlayer14forHPConsumerPC*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*D52A8D61.FarmVille2CountryEscape*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*D5EA27B7.Duolingo-LearnLanguagesforFree*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*DB6EA5DB.CyberLinkMediaSuiteEssentials*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*DolbyLaboratories.DolbyAccess*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Drawboard.DrawboardPDF*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Facebook.Facebook*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Fitbit.FitbitCoach*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*flaregamesGmbH.RoyalRevolt2*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*GAMELOFTSA.Asphalt8Airborne*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*KeeperSecurityInc.Keeper*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com.BubbleWitch3Saga*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com.CandyCrushFriends*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com.CandyCrushSaga*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com.CandyCrushSodaSaga*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com.FarmHeroesSaga*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*Nordcurrent.CookingFever*"}  | Remove-AppxPackage -Allusers
+Get-AppxPackage -PackageTypeFilter Main, Bundle, Resource | Where-Object {$_.PackageFullName -like "*king.com*"}  | Remove-AppxPackage -Allusers
+
+function Remove-StartmenueTrash ($FilePath) {
+    if ((Test-Path -Path $FilePath) -eq "True"){
+        Remove-Item -Path $FilePath
+    }
+}
+
+function SetStartmenueLayout() {
+
+Import-StartLayout -LayoutPath layout.xml -MountPath "C:\"
+
+}
+
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Amazon.lnk")
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\DeskUpdate.lnk")
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Fujitsu Extras.lnk")
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Fujitsu AuthConductor Client Basic.url")
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk")
+Remove-StartmenueTrash("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Windows 10-Update-Assistent.lnk")
+
